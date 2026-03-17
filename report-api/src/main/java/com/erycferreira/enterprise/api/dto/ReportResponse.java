2f469c3ca537deff6264eb607370f83835777f2b
Eryc Filgueiras Ferreira <eryc.ferreira@hotmail.com>
2026-03-17 09:49:13 -0300

    :recycle: refact: Atualizado responseDto para record removendo lombok
    Atualizado exceção de report não localizado

--- report-api/src/main/java/com/erycferreira/enterprise/api/dto/ReportResponse.java
+++ report-api/src/main/java/com/erycferreira/enterprise/api/dto/ReportResponse.java
@@ -3,17 +3,11 @@ package com.erycferreira.enterprise.api.dto;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
-public class ReportResponse {
-
-    private UUID id;
-    private String name;
-    private String type;
-    private String status;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
+public record ReportResponse(
+    UUID id,
+    String name,
+    String type,
+    String status,
+    LocalDateTime createdAt,
+    LocalDateTime updatedAt) {
 }
--- report-application/src/main/java/com/erycferreira/enterprise/application/usecase/StartReportProcessingUseCase.java
+++ report-application/src/main/java/com/erycferreira/enterprise/application/usecase/StartReportProcessingUseCase.java
@@ -2,6 +2,7 @@ package com.erycferreira.enterprise.application.usecase;
 
 import java.util.UUID;
 
+import com.erycferreira.enterprise.domain.exception.ReportNotFoundException;
 import com.erycferreira.enterprise.domain.model.Report;
 import com.erycferreira.enterprise.domain.repository.ReportRepository;
 
@@ -15,7 +16,7 @@ public class StartReportProcessingUseCase {
 
     public void execute(UUID reportId) {
         Report report = repository.findById(reportId)
-                .orElseThrow(() -> new IllegalArgumentException("Report not found"));
+                .orElseThrow(() -> new ReportNotFoundException(reportId.toString()));
 
         report.startProcessing();
 
